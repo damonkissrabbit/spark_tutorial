f144a4c49ea03d7b8925419bc5a324dfdfd46838
Damondongj <18226669129@163.com>
2023-08-12 21:19:16 +0800

    demo

---
+++ src/main/scala/com/damon/demo.scala
@@ -0,0 +1,36 @@
+package com.damon
+
+import org.apache.spark.sql.{SQLContext, SparkSession}
+
+
+object demo {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder()
+      .master("local[1]")
+      .appName("SparkContextExample")
+      .getOrCreate()
+
+    spark.sparkContext.setLogLevel("ERROR")
+
+    val sqlContext: SQLContext = spark.sqlContext
+
+    val df = sqlContext
+      .read
+      .options(
+        Map(
+          "inferSchema" -> "true",
+          "delimiter" -> ",",
+          "header" -> "true"
+        )
+      )
+      .csv("src/main/resources/zipcodes.csv")
+
+    df.show()
+    df.printSchema()
+
+    df.createOrReplaceTempView("TAB")
+    sqlContext.sql("select * from TAB").show(false)
+//    spark.sql("select * from TAB").show(false)
+  }
+}
